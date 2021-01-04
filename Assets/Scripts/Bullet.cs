38eba9324e40b0c86880a8a1c90e05ed11d81677
Rafa Garrido <draco032@gmail.com>
2021-01-03 21:19:52 -0400

    Add player and enemy attack system

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+	public int damage = 1;
 	public float speed = 2f;
 	public Vector2 direction;
 
@@ -11,12 +12,18 @@ public class Bullet : MonoBehaviour
 	public Color initialColor = Color.white;
 	public Color finalColor;
 
+	public GameObject explosion;
+
 	private SpriteRenderer _renderer;
+	private Rigidbody2D _rigidbody;
 	private float _startingTime;
+
+	private bool _returning;
 
 	void Awake()
 	{
 		_renderer = GetComponent<SpriteRenderer>();
+		_rigidbody = GetComponent<Rigidbody2D>();
 	}
 
 	// Start is called before the first frame update
@@ -24,22 +31,59 @@ public class Bullet : MonoBehaviour
     {
 		//  Save initial time
 		_startingTime = Time.time;
-
-		// Destroy the bullet after some time
-		Destroy(gameObject, livingTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-		//  Move object
-		Vector2 movement = direction.normalized * speed * Time.deltaTime;
-		transform.Translate(movement);
-
 		// Change bullet's color over time
 		float _timeSinceStarted = Time.time - _startingTime;
 		float _percentageCompleted = _timeSinceStarted / livingTime;
 
 		_renderer.color = Color.Lerp(initialColor, finalColor, _percentageCompleted);
-    }
+
+		if (_percentageCompleted >= 1f) {
+			Explode();
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		//  Move object
+		Vector2 movement = direction.normalized * speed;
+		_rigidbody.velocity = movement;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (_returning == false && collision.CompareTag("Player")) {
+			// Tell player to get hurt
+			collision.SendMessageUpwards("AddDamage", damage);
+			Explode();
+		}
+
+		if (_returning == true && collision.CompareTag("Enemy")) {
+			collision.SendMessageUpwards("AddDamage");
+			Explode();
+		}
+	}
+
+	public void AddDamage()
+	{
+		_returning = true;
+		direction = direction * -1f;
+	}
+
+	public void Explode()
+	{
+		speed = 0f;
+
+		_renderer.enabled = false;
+
+		if (explosion) {
+			explosion.SetActive(true);
+		}
+
+		Destroy(gameObject, 1.5f);
+	}
 }
--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -5,102 +5,122 @@ using UnityEngine;
 public class EnemyPatrol : MonoBehaviour
 {
 	public float speed = 1f;
-	public float minX;
-	public float maxX;
-	public float waitingTime = 2f;
+	public float wallAware = 0.5f;
+	public LayerMask groundLayer;
+	public float playerAware = 3f;
+	public float aimingTime = 0.5f;
+	public float shootingTime = 1.5f;
 
-	private GameObject _target;
+	private Rigidbody2D _rigidbody;
 	private Animator _animator;
 	private Weapon _weapon;
+	private AudioSource _audio;
 
+	// Movement
+	private Vector2 _movement;
+	private bool _facingRight;
+
+	private bool _isAttacking;
 
 	void Awake()
 	{
+		_rigidbody = GetComponent<Rigidbody2D>();
 		_animator = GetComponent<Animator>();
 		_weapon = GetComponentInChildren<Weapon>();
+		_audio = GetComponent<AudioSource>();
 	}
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		UpdateTarget();
-		StartCoroutine("PatrolToTarget");
+		if (transform.localScale.x < 0f) {
+			_facingRight = false;
+		} else if (transform.localScale.x > 0f) {
+			_facingRight = true;
+		}
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		Vector2 direction = Vector2.right;
+
+		if (_facingRight == false) {
+			direction = Vector2.left;
+		}
+
+		if (_isAttacking == false) {
+			if (Physics2D.Raycast(transform.position, direction, wallAware, groundLayer)) {
+				Flip();
+			}
+		}
 
 	}
 
-
-	private void UpdateTarget()
+	private void FixedUpdate()
 	{
-		// If first time, create target in the left
-		if (_target == null) {
-			_target = new GameObject("Target");
-			_target.transform.position = new Vector2(minX, transform.position.y);
-			transform.localScale = new Vector3(-1, 1, 1);
-			return;
+		float horizontalVelocity = speed;
+
+		if (_facingRight == false) {
+			horizontalVelocity = horizontalVelocity * -1f;
 		}
 
-		// If we are in the left, change target to the right
-		if (_target.transform.position.x == minX) {
-			_target.transform.position = new Vector2(maxX, transform.position.y);
-			transform.localScale = new Vector3(1, 1, 1);
+		if (_isAttacking) {
+			horizontalVelocity = 0f;
 		}
 
-		// If we are in the right, change target to the left
-		else if (_target.transform.position.x == maxX) {
-			_target.transform.position = new Vector2(minX, transform.position.y);
-			transform.localScale = new Vector3(-1, 1, 1);
+		_rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+	}
+
+	private void LateUpdate()
+	{
+		_animator.SetBool("Idle", _rigidbody.velocity == Vector2.zero);
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (_isAttacking == false && collision.CompareTag("Player")) {
+			StartCoroutine("AimAndShoot");
 		}
 	}
 
-	private IEnumerator PatrolToTarget()
+	private void Flip()
 	{
-		// Coroutine to move the enemy
-		while (Vector2.Distance(transform.position, _target.transform.position) > 0.05f) {
+		_facingRight = !_facingRight;
+		float localScaleX = transform.localScale.x;
+		localScaleX = localScaleX * -1f;
+		transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+	}
 
-			// Update animator
-			_animator.SetBool("Idle", false);
+	private IEnumerator AimAndShoot()
+	{
+		_isAttacking = true;
 
+		yield return new WaitForSeconds(aimingTime);
 
-			// let's move to the target
-			Vector2 direction = _target.transform.position - transform.position;
-			float xDirection = direction.x;
-
-			transform.Translate(direction.normalized * speed * Time.deltaTime);
-
-			// IMPORTANT
-			yield return null;
-		}
-
-		// At this point, i've reached the target, let's set our position to the target's one
-		Debug.Log("Target reached");
-		transform.position = new Vector2(_target.transform.position.x, transform.position.y);
-		UpdateTarget();
-
-		// Update animator
-		_animator.SetBool("Idle", true);
-
-		// Shoot
 		_animator.SetTrigger("Shoot");
 
+		yield return new WaitForSeconds(shootingTime);
 
-		// And let's wait for a moment
-		Debug.Log("Waiting for " + waitingTime + " seconds");
-		yield return new WaitForSeconds(waitingTime); // IMPORTANT
-
-		// once waited, let's restore the patrol behaviour
-		Debug.Log("Waited enough, let's update the target and move again");
-		StartCoroutine("PatrolToTarget");
+		_isAttacking = false;
 	}
 
 	void CanShoot()
 	{
 		if (_weapon != null) {
 			_weapon.Shoot();
+			_audio.Play();
 		}
+	}
+
+	private void OnEnable()
+	{
+		_isAttacking = false;
+	}
+
+	private void OnDisable()
+	{
+		StopCoroutine("AimAndShoot");
+		_isAttacking = false;
 	}
 }
